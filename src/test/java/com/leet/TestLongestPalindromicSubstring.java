fdb98c07560c4d7e7b64f1096fe82347b751bc49
August Raack <sum-rock@rockyhillhouse.org>
2024-08-20 20:53:03 -0500

    Better test pattern

--- src/test/java/com/leet/TestLongestPalindromicSubstring.java
+++ src/test/java/com/leet/TestLongestPalindromicSubstring.java
@@ -15,27 +15,25 @@ public class TestLongestPalindromicSubstring extends TestCase {
     return new TestSuite(TestLongestPalindromicSubstring.class);
   }
 
+  private LongestPalindromicSubstring puzzle = new LongestPalindromicSubstring(false);
+
   public void testBananas() {
-    LongestPalindromicSubstring puzzle = new LongestPalindromicSubstring(false);
     assertTrue(puzzle.solve(new String[] { "bananas" }).equals("anana"));
   }
 
   public void testRepeatingChars() {
-    LongestPalindromicSubstring puzzle = new LongestPalindromicSubstring(false);
     assertTrue(puzzle.solve(new String[] { "aaaaaaa" }).equals("aaaaaaa"));
     assertTrue(puzzle.solve(new String[] { "baaaaaaas" }).equals("aaaaaaa"));
     assertTrue(puzzle.solve(new String[] { "baaaaaaab" }).equals("baaaaaaab"));
   }
 
   public void testRepeatingDigraph() {
-    LongestPalindromicSubstring puzzle = new LongestPalindromicSubstring(false);
     assertTrue(puzzle.solve(new String[] { "babababab" }).equals("babababab"));
     assertTrue(puzzle.solve(new String[] { "bababab" }).equals("bababab"));
     assertTrue(puzzle.solve(new String[] { "xbabababy" }).equals("bababab"));
   }
 
   public void testLongSequence() {
-    LongestPalindromicSubstring puzzle = new LongestPalindromicSubstring(false);
     String testString = "abcdefghijklmnopqrstuvvutsrqponmlkjihgfedcba";
     assertTrue(puzzle.solve(new String[] { testString }).equals(testString));
   }
